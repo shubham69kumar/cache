2213641e3ad67960b6a00cf089da7e3f5c2c16ff
shubham69kumar <shubham69kumar0903@gmail.com>
2022-01-31 12:19:36 +0530

    change

--- cache/urls.py
+++ cache/urls.py
@@ -17,10 +17,14 @@ from re import search
 from django.contrib import admin
 from django.urls import path
 from cns_project import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.cache,name='cache'),
     path('search/',views.search,name='search-url'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     
 ]
